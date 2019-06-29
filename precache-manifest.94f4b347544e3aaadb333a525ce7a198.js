e0d7dfad487336e5fa439bbb210cc8299b208358
Harshan Morawaka <=>
2019-06-29 12:07:14 +0530

    build project

---
+++ precache-manifest.94f4b347544e3aaadb333a525ce7a198.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6117984f939fa05dd0e0306eff7b8c82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a745e190ad0b55c4426",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "9885fefe9aec69fa3c9d",
+    "url": "/static/js/2.547e2c78.chunk.js"
+  },
+  {
+    "revision": "6a745e190ad0b55c4426",
+    "url": "/static/js/main.b49567ca.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
