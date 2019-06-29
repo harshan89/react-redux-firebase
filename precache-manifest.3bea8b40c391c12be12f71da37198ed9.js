529315a170af59429d007b2a291297888239a68e
Harshan Morawaka <=>
2019-06-29 13:22:00 +0530

    Updates

---
+++ precache-manifest.3bea8b40c391c12be12f71da37198ed9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c6e43664d2362563b161a962e1b2bea",
+    "url": "/react-redux-firebase/index.html"
+  },
+  {
+    "revision": "ab520fdf21076b6074a7",
+    "url": "/react-redux-firebase/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "9885fefe9aec69fa3c9d",
+    "url": "/react-redux-firebase/static/js/2.547e2c78.chunk.js"
+  },
+  {
+    "revision": "ab520fdf21076b6074a7",
+    "url": "/react-redux-firebase/static/js/main.6d348ab9.chunk.js"
+  },
+  {
+    "revision": "3a37e8dbdce109778be3",
+    "url": "/react-redux-firebase/static/js/runtime~main.ee7e5a22.js"
+  }
+]);
