16d926e47be398506e41d7bc2e7fdb6ec5322cfd
Harshan Morawaka <=>
2019-07-23 16:38:13 +0530

    project summary more moduler

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Dashboard from './components/dashboard/Dashboard'
+import ProjectDetails from './components/projects/ProjectDetails'
 // import './App.css';
 
 function App() {
@@ -9,7 +10,8 @@ function App() {
     <BrowserRouter>
       <Navbar />
       <Switch>
-        <Route path='/' component={Dashboard} />
+        <Route exact path='/' component={Dashboard} />
+        <Route path='/project/:id' component={ProjectDetails} />
       </Switch>
     </BrowserRouter>
   );
--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Notifications from './Notifications'
 import ProjectList from '../projects/ProjectList'
+
 export default class Dashboard extends Component {
     render() {
         return (
